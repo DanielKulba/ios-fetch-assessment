a6ae75cc24cea31d7da638cc03b8191aa5d591a8
DanielKulba <danielkulbachenko@gmail.com>
2024-01-12 14:58:08 -0600

    Initial Commit

---
+++ fetch assessment/fetch_assessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_assessmentApp.swift
+//  fetch assessment
+//
+//  Created by Daniel Kulbachenko on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_assessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
